e7093cf5d5a2a79130e723dc09a159310ef65016
Robin Gabriel Camacho <robingabrielcamacho@gmail.com>
2024-10-23 19:29:22 -0700

    t

--- nav.js
+++ nav.js
@@ -140,5 +140,60 @@ document.addEventListener('DOMContentLoaded', () => {
         shuffleGallery();
         adjustGrid();
     });
-    
+    const gallery = document.getElementById("gallery");
+
+    window.onmousemove = (e) => {
+      const mouseX = e.clientX,
+        mouseY = e.clientY;
+
+      const xDecimal = mouseX / window.innerWidth,
+        yDecimal = mouseY / window.innerHeight;
+
+      const maxX = gallery.offsetWidth - window.innerWidth,
+        maxY = gallery.offsetHeight - window.innerHeight;
+
+      const panX = maxX * xDecimal * -1,
+        panY = maxY * yDecimal * -1;
+
+      gallery.animate(
+        {
+          transform: `translate(${panX}px, ${panY}px)`,
+        },
+        {
+          duration: 4000,
+          fill: "forwards",
+          easing: "ease",
+        }
+      );
+    };
+
+    const radius = 300,
+      maxScale = 3,
+      blocks = document.querySelectorAll(".block"),
+      radius2 = radius * radius,
+      container = document.querySelector("#gallery");
+
+    blocks.forEach((block) => {
+      let b = block.getBoundingClientRect();
+      (block.cx = b.left + b.width / 2 + window.pageXOffset),
+        (block.cy = b.top + b.height / 2 + window.pageYOffset);
+
+      block.tween = gsap
+        .to(block, { scale: maxScale, ease: "power1.in", paused: true })
+        .progress(1)
+        .progress(0);
+    });
+
+    document.addEventListener("mousemove", (e) => {
+      let i = blocks.length,
+        dx,
+        dy,
+        block;
+      while (i--) {
+        block = blocks[i];
+        dx = (block.cx - e.pageX) ** 2;
+        dy = (block.cy - e.pageY) ** 2;
+        block.tween.progress(1 - (dx + dy) / radius2);
+      }
+    });
 });
